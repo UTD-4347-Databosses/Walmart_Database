14aa93c789ac73a60ecba7454d0e1431fac104c1
Mattia <mattiamoren@yahoo.com>
2024-07-14 09:00:46 -0500

    Fixed image and updated redirect links

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 
 def index():
-    return render_template('index.html')
+    return render_template('walmart.html')
 
 @bp.route('/inventory')
 def inventory():
